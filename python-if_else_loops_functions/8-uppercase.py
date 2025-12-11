7d640dd07015846542425c61d8f52755e81213c2
RoyalAskarov <royalaskar42@gmail.com>
2025-12-11 22:25:33 +0000

    cm88

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("{}".format(str.upper()))
+    num = ord("a") - ord("A")
+    str += "\n"
+    for i in range(len(str)):
+        if str[i].isalpha() and str[i].islower():
+            print("{}".format(chr(ord(str[i]) - num)), end="")
+        else:
+            print("{}".format(str[i]), end="")
