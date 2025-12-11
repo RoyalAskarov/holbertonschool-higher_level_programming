27a596907b713dd4700531f9d49c16e29a656b43
RoyalAskarov <royalaskar42@gmail.com>
2025-12-11 19:27:13 +0000

    cm6.1

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ x = 0
 while x < 8:
     y = x + 1
     while y < 10:
-        print("{}{}, ".format(x, y), end = '')
+        print("{}{}, ".format(x, y), end='')
         y += 1
     x += 1
 print(89)
