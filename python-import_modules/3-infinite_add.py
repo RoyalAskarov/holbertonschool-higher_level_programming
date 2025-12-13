f5a001dde27a9814f794f0019bfb9048d20ce999
RoyalAskarov <royalaskar42@gmail.com>
2025-12-13 07:28:44 +0000

    cm3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+    
+    # Loop through arguments starting from index 1
+    # (Index 0 is the script name, so we skip it)
+    for arg in sys.argv[1:]:
+        total += int(arg)
+        
+    print(total)
