5188714d272a0bccc21905fd23d53f5bd950bcea
RoyalAskarov <royalaskar42@gmail.com>
2025-12-16 12:58:04 +0400

    cm1.1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    mylist = my_list.copy()
+    for i in range(len(mylist)):
+        if mylist[i] == search:
+            mylist[i] = replace
+    return mylist
