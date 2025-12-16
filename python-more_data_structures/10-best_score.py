7193cb733620c578210954070563d0af3dee4e41
RoyalAskarov <royalaskar42@gmail.com>
2025-12-16 15:10:44 +0400

    cm10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
     key = ""
     a = 0
     for keyy, value in a_dictionary.items():
@@ -7,3 +9,4 @@ def best_score(a_dictionary):
             a = value
             key = keyy
     return key
+
