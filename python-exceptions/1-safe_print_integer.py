6df6cff7f556497603a32f046f078138d309b320
Royal Askarov <royalaskar42@gmail.com>
2025-12-22 09:20:19 +0400

    cm1.1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if isinstance(value, int):
-        print(value)
+    try:
+        print("{:d}".format(value))
         return True
-    else:
+    except (ValueError, TypeError):
         return False
