7b711a3056bde0aa545887cc9c5ee8d29956879d
Royal Askarov <royalaskar42@gmail.com>
2026-01-08 10:53:43 +0400

    cm1.2

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,6 +27,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
+        self.__width = width
 
     @height.setter
     def height(self, height):
@@ -34,3 +35,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
+        self.__height = height
