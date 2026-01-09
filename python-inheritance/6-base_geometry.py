008b67c96b633cbebbc8c5d69684aec86a57b99f
Royal Askarov <royalaskar42@gmail.com>
2026-01-09 11:17:35 +0400

    cm6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry with an area method.
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing base geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
